fb2c9b2b34b6b4bff45c5322adffc285375c22c5
Greg <leblanc.greg@gmail.com>
2018-02-28 00:16:47 +0100

    Created player class file only. Not functional

--- PlayerClass.py
+++ PlayerClass.py
@@ -2,3 +2,4 @@
 # Will store properties of a player such as:
 # Name, number of games played, average rounds, etc...
 # would like to hook the game up to a DB to try that out & store / retrieve player data each time game is loaded.
+# what other features should a player have??
